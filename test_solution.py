bef79dfa0ad38361c4fe0db5b8784a07346f8463
Peninah <pennie.yna@gmail.com>
2017-03-06 16:58:27 +0300

    second commitment

--- test_solution.py
+++ test_solution.py
@@ -1,31 +1,31 @@
 import unittest
 from t_s import solution
 
-class TestSolution (unittest.Testcacse):
+class TestSolution (unittest.TestCase):
 	def test_addition (Self):
 		"""the tests result should be the addition of the two numbers, 100 + 200"""
-		self.AsserTrue (solution (100,200, "+"), 300)
+		self.assertTrue (solution (100,200, "+"), 300)
 
-	def test_substraction (self):
-		"""the test result should produce the difference between the numbers, 100 - 200"""
-		self.AsserTrue (solution (100,200, "-"), -100)
+	def test_subtraction (self):
+		"""the test result should produce the difference between the numbers, 200 - 100"""
+		self.assertTrue (solution (200,100, "-"), 100)
 
 
 	def test_multiplication (self):
 		"""the test result should produce the multiplication of the numbers, 100 * 200"""
-		self.AsserTrue (solution (100,200, "*"), 20000)
+		self.assertTrue (solution (100,200, "*"), 20000)
 
 
 	def test_division (self):
 		"""the test result should produce the division of the numbers, 100 / 200"""
-		self.AsserTrue (solution (100,200, "*"), 0.5)
+		self.assertTrue (solution (100,200, "*"), 0.5)
 
 	def test_negative_number (self):
 		"""the test result should produce negative number"""
-		self.AsserTrue (solution (4,5,-8, "+"),-8 )
+		self.assertTrue (solution (4,5,-8, "+"),-8 )
 
 
 
 
 if __name__ == "__main__":
-unittest.main()
+	unittest.main()
